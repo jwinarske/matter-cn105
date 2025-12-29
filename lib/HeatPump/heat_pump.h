578e2a8b37d08cfce141d90c7192680b9f043262
Joel Winarske <joel.winarske@gmail.com>
2025-12-29 12:49:38 -0800

    add copyright
    Signed-off-by: Joel Winarske <joel.winarske@gmail.com>

--- lib/HeatPump/heat_pump.h
+++ lib/HeatPump/heat_pump.h
@@ -1,5 +1,6 @@
 /*
   HeatPump.h - Mitsubishi Heat Pump control library for Arduino
+  Copyright (c) 2025 Joel Winarske.  All right reserved.
   Copyright (c) 2017 Al Betschart.  All right reserved.
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
